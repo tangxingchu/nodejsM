72954a98e8f32506279f45240711529f9accdaa8
tangxingchu <51697550@qq.com>
2017-04-20 18:40:11 +0800

    '代码提交'

---
+++ node-pro1/app/appConver.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var AppConver = function() {
+
+};
+
+AppConver.prototype.createApp = function() {
+	console.log('app');
+}
+
+module.exports = AppConver;
---
+++ node-pro1/app/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const module1 = require('./scrollViewExample.json');
+const AppConver = require('./appConver.js');
+
+function travel(module, callback) {
+	let metaType = module.metaType;
+	console.log(AppConver.prototype['create' + metaType].apply(AppConver, []));
+}
+
+travel(module1,  function(str){
+	console.log(str);
+});
+
